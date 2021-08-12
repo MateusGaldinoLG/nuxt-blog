6b406439ee72b52e09c9bcfaab3676f9a36eb8bb
Mateus Galdino <mateuslguilherme@gmail.com>
2021-08-11 22:45:23 -0300

    Updates

--- _nuxt/static/1628648926/blog/Appresentation/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-blog\u002F_nuxt\u002Fstatic\u002F1628648926",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002FAppresentation",config:{_app:{basePath:"\u002Fnuxt-blog\u002F",assetsPath:"\u002Fnuxt-blog\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e1c26de9"}}}}(null));
