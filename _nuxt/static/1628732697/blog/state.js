5f125d8df8d82379f7a467b5901edf9606d8230f
Mateus Galdino <mateuslguilherme@gmail.com>
2021-08-13 16:21:48 -0300

    Updates

--- _nuxt/static/1628732697/blog/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-blog\u002F_nuxt\u002Fstatic\u002F1628732697",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog",config:{_app:{basePath:"\u002Fnuxt-blog\u002F",assetsPath:"\u002Fnuxt-blog\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ea643c21"}}}}(null));
---
+++ _nuxt/static/1628882103/blog/Appresentation/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-blog\u002F_nuxt\u002Fstatic\u002F1628882103",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002FAppresentation",config:{_app:{basePath:"\u002Fnuxt-blog\u002F",assetsPath:"\u002Fnuxt-blog\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d2474813"}}}}(null));
